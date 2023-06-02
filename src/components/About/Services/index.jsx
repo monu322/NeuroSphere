264c028069a17ede2ada61f6219ab0c4393e98a7
Monu John <monu.k.john6597@gmail.com>
2023-06-02 15:13:25 +0100

    added services to about page

--- src/components/About/Services/index.jsx
+++ src/components/About/Services/index.jsx
@@ -11,7 +11,7 @@ const Services = () => {
                 <div className={`item wow fadeInUp ${index !== ServicesData.length - 1 && 'md-mb50'}`} data-wow-delay={`${0.3 * index + 0.3}s`}>
                   <span className={`icon ${service.iconClass}`}></span>
                   <h6>{ service.title }</h6>
-                  <p>{ service.details }</p>
+                  <p>{ service.description }</p>
                 </div>
               </div>
             ))
