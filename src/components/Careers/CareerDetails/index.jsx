b8c7fb1b11fc022ab2293c8f1f1f0b7032ce17c4
Monu John <monu.k.john6597@gmail.com>
2023-06-15 22:41:38 +0100

    careers page update

--- src/components/Careers/CareerDetails/index.jsx
+++ src/components/Careers/CareerDetails/index.jsx
@@ -1,6 +1,6 @@
 const Careers = () => {
   return (
-    <section className="contact section-padding">
+    <section className="section-padding">
       <div className="container">
         <h4>Send Us Your Resume</h4>
         <p className="mb-10">We will get in touch with you</p>
--- src/pages/careers/index.jsx
+++ src/pages/careers/index.jsx
@@ -10,7 +10,7 @@ const Index = () => {
   }, []);
 
   return (
-    <CareersLayout footerClass="bg-gray">
+    <CareersLayout>
       <CareerDetails />
     </CareersLayout>
   );
