78f873c0df9b86d3f789064d417a9ca35186eefd
Monu John <monu.k.john6597@gmail.com>
2023-06-15 22:49:18 +0100

    build fix

--- src/pages/careers/index.jsx
+++ src/pages/careers/index.jsx
@@ -11,12 +11,8 @@ const Index = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
     <CareersLayout>
-=======
-    <CareersLayout footerClass="bg-gray">
       <Header />
->>>>>>> b5424361617c5672cb1dd777e8eed106897e3549
       <CareerDetails />
     </CareersLayout>
   );
