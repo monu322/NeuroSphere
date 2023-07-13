3d7fb0b09c11b76d81102583c8c6338c4f18e656
abishek-william <104523659+abishek-william@users.noreply.github.com>
2023-07-13 13:15:09 +0530

    responsive-fix-abishek

--- src/components/Home3/Works/index.jsx
+++ src/components/Home3/Works/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, Fragment } from "react";
-import Link from 'next/link';
-import WorksData from '../../../data/Home3/Works.json';
+import Link from "next/link";
+import WorksData from "../../../data/Home3/Works.json";
 import initIsotope from "../../../common/initIsotope";
 
 const Works = () => {
@@ -12,9 +12,9 @@ const Works = () => {
   }, []);
 
   return (
-    <section className="ui-works section-padding home-portfolio-section">
-      <div className="container">
-        <div className="row mt-4 pt-4">
+    <section className="ui-works section-padding home-portfolio-section mt-0">
+      <div className="container mt-0">
+        <div className="row mt-0 pt-4">
           <div className="col-lg-7">
             <div className="main-header mt-4 pt-4">
               <h6>Our Portfolio</h6>
@@ -26,49 +26,57 @@ const Works = () => {
           {/* filter links */}
           <div className="filtering text-center col-12">
             <div className="filter">
-              {
-                WorksData.filters.map((filter, index) => (
-                  <span data-filter={filter.operator} className={filter.operator === '*'?'active':''} key={index}>{ filter.title }</span>
-                ))
-              }
+              {WorksData.filters.map((filter, index) => (
+                <span
+                  data-filter={filter.operator}
+                  className={filter.operator === "*" ? "active" : ""}
+                  key={index}
+                >
+                  {filter.title}
+                </span>
+              ))}
             </div>
           </div>
           {/* gallery */}
           <div className="gallery full-width">
-            {
-              WorksData.works.map((work, index) => (
-                <div className={`col-lg-4 col-md-6 items wow fadeInUp ${work.type}`} data-wow-delay=".4s" key={index}>
-                  <div className="item-img imago wow">
-                    <Link href={`/works/${index+1}`}>
-                      <a href={`/works/${index+1}`} className="popimg">
-                        <img src={work.img} alt="image" />
-                        <div className="item-img-overlay"></div>
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="cont">
-                    <h6>{ work.title }</h6>
-                    <span>
-                      {
-                        work.tags.map((tag, idx) => (
-                          <Fragment key={idx}>
-                            <Link href="#0">
-                              <a>{ tag }</a>
-                            </Link>
-                            { idx !== work.tags.length - 1 && <>, </> }
-                          </Fragment>
-                        ))
-                      }
-                    </span>
-                  </div>
+            {WorksData.works.map((work, index) => (
+              <div
+                className={`col-lg-4 col-md-6 items wow fadeInUp ${work.type}`}
+                data-wow-delay=".4s"
+                key={index}
+              >
+                <div className="item-img imago wow">
+                  <Link href={`/works/${index + 1}`}>
+                    <a href={`/works/${index + 1}`} className="popimg">
+                      <img
+                        src={work.img}
+                        alt="image"
+                        className="img-fluid custom-img"
+                      />
+                      <div className="item-img-overlay"></div>
+                    </a>
+                  </Link>
                 </div>
-              ))
-            }
+                <div className="cont">
+                  <h6>{work.title}</h6>
+                  <span>
+                    {work.tags.map((tag, idx) => (
+                      <Fragment key={idx}>
+                        <Link href="#0">
+                          <a>{tag}</a>
+                        </Link>
+                        {idx !== work.tags.length - 1 && <>, </>}
+                      </Fragment>
+                    ))}
+                  </span>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
