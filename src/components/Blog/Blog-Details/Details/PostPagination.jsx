83fc908db601dfebc17eb3995d0a3ca08e178c05
sherin koshy <sherinkoshy@sherins-MacBook-Air.local>
2023-07-19 17:11:33 +0530

    pagination implemented

--- src/components/Blog/Blog-Details/Details/PostPagination.jsx
+++ src/components/Blog/Blog-Details/Details/PostPagination.jsx
@@ -1,25 +1,50 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-const PostPagination = ({ SingleBlog }) => {
+const PostPagination = ({
+  SingleBlog,
+  currentIndex,
+  totalBlogs,
+  nextBlog,
+  nextIndex,
+  previousBlog,
+  previousIndex,
+  blogs,
+}) => {
+  function convertToSlug(Text) {
+    return Text.toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  }
+
   return (
     <div className="pagination">
       <span>
-        <Link href="#0">
+        <Link
+          href={`/blog/${convertToSlug(
+            nextBlog?.title
+          )}-${previousIndex}-${totalBlogs}-${previousBlog?.id}`}
+        >
           <a>Prev Post</a>
         </Link>
       </span>
       <span className="icon">
         <Link href="/blog">
-          <a><i className="fas fa-th-large"></i></a>
+          <a>
+            <i className="fas fa-th-large"></i>
+          </a>
         </Link>
       </span>
       <span className="text-right">
-        <Link href="#0">
+        <Link
+          href={`/blog/${convertToSlug(
+            nextBlog?.title
+          )}-${nextIndex}-${totalBlogs}-${nextBlog?.id}`}
+        >
           <a>Next Post</a>
         </Link>
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default PostPagination
+export default PostPagination;
--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -4,10 +4,18 @@ import BlogLayout from "../../layouts/blog";
 import Details from "../../components/Blog/Blog-Details/Details";
 // import blogs from "../../data/blogs";
 import Header from "../../components/Blog/Blog-Details/Header";
-import { Timestamp, doc, getDoc } from "firebase/firestore";
+import {
+  Timestamp,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import db from "../../config/fire-config";
 
-const Index = ({ blog }) => {
+const Index = ({ blog, currentIndex, totalBlogs, blogs }) => {
   useEffect(() => {
     let body = document.querySelector("body");
     body.classList.add("bg-gr");
@@ -17,7 +25,12 @@ const Index = ({ blog }) => {
   return (
     <BlogLayout footerClass="bg-gray">
       {/* <Header /> */}
-      <Details blog={blog} />
+      <Details
+        blog={blog}
+        blogs={blogs}
+        currentIndex={currentIndex > totalBlogs ? 0 : currentIndex}
+        totalBlogs={totalBlogs}
+      />
     </BlogLayout>
   );
 };
@@ -26,10 +39,25 @@ export default Index;
 
 export async function getServerSideProps(context) {
   const val = context.params.id.split("-");
+  const totalBlogs = Number(val[val.length - 2]);
+  const currentIndex = Number(val[val.length - 3]);
+  const blogCollection = collection(db, "blogs");
+  const q = query(blogCollection, orderBy("postedDate", "desc"));
+  const querySnapshot = await getDocs(q);
+  const allBlogs = [];
+  querySnapshot.forEach((doc) => {
+    const docData = doc.data();
+    const postedDate =
+      docData.postedDate instanceof Timestamp
+        ? docData.postedDate.toDate()
+        : docData.postedDate;
+    docData.postedDate = postedDate.getTime();
+    allBlogs.push(docData);
+  });
+
   const id = val[val.length - 1];
   const docRef = doc(db, "blogs", id);
   const docSnap = await getDoc(docRef);
-
   const docData = docSnap.data();
   const postedDate =
     docData.postedDate instanceof Timestamp
@@ -39,6 +67,9 @@ export async function getServerSideProps(context) {
   return {
     props: {
       blog: docData,
+      blogs: allBlogs,
+      currentIndex,
+      totalBlogs,
     },
   };
 }
