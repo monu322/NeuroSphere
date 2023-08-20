32abcf219b442fb1ae72da7f925703f7003888b7
sherin koshy <sherinkoshy@sherins-MacBook-Air.local>
2023-08-20 14:58:48 +0530

    shimmer ui created for client prospecting

--- src/components/Admin/Client/ClientProspect.jsx
+++ src/components/Admin/Client/ClientProspect.jsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import React, { useState } from "react";
+import ClientShimmer from "../../ShimmerUi/ClientShimmer";
 
 const ClientProspect = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [company, setCompany] = useState();
   const [location, setLocation] = useState();
   const [category, setCategory] = useState();
   const [employees, setEmployees] = useState();
+  const [funding, setFunding] = useState();
+  const [revenue, setRevenue] = useState();
   console.log(company, location, category, employees);
   return (
     <div className="container mt-2">
@@ -37,150 +41,166 @@ const ClientProspect = () => {
                 value={employees}
                 icon="user"
               />
+              <SetField
+                handleChange={(e) => setFunding(e.target.value)}
+                title="Funding"
+                value={funding}
+                icon="suitcase"
+              />
+              <SetField
+                handleChange={(e) => setRevenue(e.target.value)}
+                title="Revenue"
+                value={revenue}
+                icon="dollar-sign"
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className="row text-dark">
-        <div className="col-lg-12">
-          <div className="bg-white">
-            <table className="table__style">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Title</th>
-                  <th>Company</th>
-                  <th>Location</th>
-                  <th>Employees</th>
-                  <th>Email</th>
-                  <th>Industry</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className="text-primary">Robert Gates</td>
-                  <td>Principal Could Solution Architect</td>
-                  <td>
-                    <div className="d-flex align-items-center">
-                      <div className="company-logo mr-2">
-                        <i className="fab fa-microsoft company-logo"></i>
-                      </div>
-                      <div className="links">
-                        <p className="company-name">Microsoft</p>
-                        <div className="company-links">
-                          <a href="#">
-                            <i className="fab fa-linkedin mr-1 item-1"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-facebook-square mr-1 item-2"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-twitter-square item-3"></i>
-                          </a>
+      {isLoading ? (
+        <ClientShimmer />
+      ) : (
+        <div className="row text-dark">
+          <div className="col-lg-12">
+            <div className="bg-white">
+              <table className="table__style">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Title</th>
+                    <th>Company</th>
+                    <th>Location</th>
+                    <th>Employees</th>
+                    <th>Email</th>
+                    <th>Industry</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="text-primary">Robert Gates</td>
+                    <td>Principal Could Solution Architect</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <div className="company-logo mr-2">
+                          <i className="fab fa-microsoft company-logo"></i>
+                        </div>
+                        <div className="links">
+                          <p className="company-name">Microsoft</p>
+                          <div className="company-links">
+                            <a href="#">
+                              <i className="fab fa-linkedin mr-1 item-1"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-facebook-square mr-1 item-2"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-twitter-square item-3"></i>
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>Charleston</td>
-                  <td>22600</td>
-                  <td>robert@gmail.com</td>
-                  <td>Information Technology</td>
-                </tr>
-                <tr>
-                  <td className="text-primary">Jeff Weiner</td>
-                  <td>Executive Chairman</td>
-                  <td>
-                    {" "}
-                    <div className="d-flex align-items-center">
-                      <div className="company-logo mr-2">
-                        <i className="fab fa-linkedin company-logo"></i>
-                      </div>
-                      <div className="links">
-                        <p className="company-name">Linkedin</p>
-                        <div className="company-links">
-                          <a href="#">
-                            <i className="fab fa-linkedin mr-1 item-1"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-facebook-square mr-1 item-2"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-twitter-square item-3"></i>
-                          </a>
+                    </td>
+                    <td>Charleston</td>
+                    <td>22600</td>
+                    <td>robert@gmail.com</td>
+                    <td>Information Technology</td>
+                  </tr>
+                  <tr>
+                    <td className="text-primary">Jeff Weiner</td>
+                    <td>Executive Chairman</td>
+                    <td>
+                      {" "}
+                      <div className="d-flex align-items-center">
+                        <div className="company-logo mr-2">
+                          <i className="fab fa-linkedin company-logo"></i>
+                        </div>
+                        <div className="links">
+                          <p className="company-name">Linkedin</p>
+                          <div className="company-links">
+                            <a href="#">
+                              <i className="fab fa-linkedin mr-1 item-1"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-facebook-square mr-1 item-2"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-twitter-square item-3"></i>
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>United States</td>
-                  <td>2600</td>
-                  <td>jeff@gmail.com</td>
-                  <td>Information Technology</td>
-                </tr>
-                <tr>
-                  <td className="text-primary">Mike Bloomberg</td>
-                  <td>Founder</td>
-                  <td>
-                    {" "}
-                    <div className="d-flex align-items-center">
-                      <div className="company-logo mr-2">
-                        <i className="fab fa-microsoft company-logo"></i>
-                      </div>
-                      <div className="links">
-                        <p className="company-name">Bloomberg</p>
-                        <div className="company-links">
-                          <a href="#">
-                            <i className="fab fa-linkedin mr-1 item-1"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-facebook-square mr-1 item-2"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-twitter-square item-3"></i>
-                          </a>
+                    </td>
+                    <td>United States</td>
+                    <td>2600</td>
+                    <td>jeff@gmail.com</td>
+                    <td>Information Technology</td>
+                  </tr>
+                  <tr>
+                    <td className="text-primary">Mike Bloomberg</td>
+                    <td>Founder</td>
+                    <td>
+                      {" "}
+                      <div className="d-flex align-items-center">
+                        <div className="company-logo mr-2">
+                          <i className="fab fa-microsoft company-logo"></i>
+                        </div>
+                        <div className="links">
+                          <p className="company-name">Bloomberg</p>
+                          <div className="company-links">
+                            <a href="#">
+                              <i className="fab fa-linkedin mr-1 item-1"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-facebook-square mr-1 item-2"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-twitter-square item-3"></i>
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>New York</td>
-                  <td>28000</td>
-                  <td>mike@gmail.com</td>
-                  <td>Financial Services</td>
-                </tr>
-                <tr>
-                  <td className="text-primary">Robert Gates</td>
-                  <td>Principal Could Solution Architect</td>
-                  <td>
-                    {" "}
-                    <div className="d-flex align-items-center">
-                      <div className="company-logo mr-2">
-                        <i className="fab fa-facebook company-logo"></i>
-                      </div>
-                      <div className="links">
-                        <p className="company-name">FaceBook</p>
-                        <div className="company-links">
-                          <a href="#">
-                            <i className="fab fa-linkedin mr-1 item-1"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-facebook-square mr-1 item-2"></i>
-                          </a>
-                          <a href="#">
-                            <i className="fab fa-twitter-square item-3"></i>
-                          </a>
+                    </td>
+                    <td>New York</td>
+                    <td>28000</td>
+                    <td>mike@gmail.com</td>
+                    <td>Financial Services</td>
+                  </tr>
+                  <tr>
+                    <td className="text-primary">Robert Gates</td>
+                    <td>Principal Could Solution Architect</td>
+                    <td>
+                      {" "}
+                      <div className="d-flex align-items-center">
+                        <div className="company-logo mr-2">
+                          <i className="fab fa-facebook company-logo"></i>
+                        </div>
+                        <div className="links">
+                          <p className="company-name">FaceBook</p>
+                          <div className="company-links">
+                            <a href="#">
+                              <i className="fab fa-linkedin mr-1 item-1"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-facebook-square mr-1 item-2"></i>
+                            </a>
+                            <a href="#">
+                              <i className="fab fa-twitter-square item-3"></i>
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </td>
-                  <td>Charleston</td>
-                  <td>22600</td>
-                  <td>robert@gmail.com</td>
-                  <td>Information Technology</td>
-                </tr>
-              </tbody>
-            </table>
+                    </td>
+                    <td>Charleston</td>
+                    <td>22600</td>
+                    <td>robert@gmail.com</td>
+                    <td>Information Technology</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
@@ -191,9 +211,12 @@ const SetField = ({ handleChange, title, value, icon }) => {
     <div className="client__filter mr-2 p-1">
       <a>
         <i className={`icon fas fa-${icon} mr-1`}></i>
-        <label className="mb-0">{title}</label>
+        <label htmlFor={title} className="mb-0">
+          {title}
+        </label>
       </a>
       <input
+        id={title}
         className="input"
         type="text"
         onChange={handleChange}
